dff4abb2b5f272050aafe8e74d34ef6796fa42af
Brian Firestone <bfirestone2339@gmail.com>
2021-05-03 18:31:28 -0700

    Add additional parent container and Bootstrap styling

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -4,10 +4,12 @@ import { Container } from 'react-bootstrap';
 class Gif extends Component {
     render() {
         return (
-            <Container>
-                <img className="rounded" alt='' src={this.props.src} />
-                <p>{this.props.title}</p>
-                <a rel='noreferrer' target='_blank' href={this.props.url}>View on Giphy</a>
+            <Container className="card my-3" style={{background: "rgba(199, 199, 199, 0.5)", width: "50%"}}>
+                <Container className="card-body" >
+                    <img className="rounded img-fluid" alt='' src={this.props.src} />
+                    <h6 className="card-title m-1">{this.props.title}</h6>
+                    <a className="" rel='noreferrer' target='_blank' href={this.props.url}>View on Giphy</a>
+                </Container>
             </Container>
         );
     }
