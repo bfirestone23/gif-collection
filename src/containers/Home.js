fe0d564c0793cfb388f2058b345e4d004b0750e1
Brian Firestone <bfirestone2339@gmail.com>
2021-05-04 09:51:12 -0700

    Replace Container components with divs

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import GifListContainer from './GifListContainer'
-import { Container } from 'react-bootstrap';
 
 export default class Home extends Component {
     render() {
         return (
-            <Container>
+            <div>
                 <GifListContainer />
-            </Container>
+            </div>
         )
     }
 }
