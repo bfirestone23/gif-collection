c173fe8cd903672387f650ed01e6de35bc33be44
Brian Firestone <bfirestone2339@gmail.com>
2021-05-04 09:48:32 -0700

    Remove title and link, wrap img in link, replace Container components with divs

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
 
 class Gif extends Component {
     render() {
         return (
-            <Container className="card my-3" style={{background: "rgba(199, 199, 199, 0.5)", width: "50%"}}>
-                <Container className="card-body" >
-                    <img className="rounded img-fluid" alt='' src={this.props.src} />
-                    <h6 className="card-title m-1">{this.props.title}</h6>
-                    <a className="" rel='noreferrer' target='_blank' href={this.props.url}>View on Giphy</a>
-                </Container>
-            </Container>
+            <div className="gif">
+                <div>
+                    <a rel='noreferrer' target='_blank' href={this.props.url}>
+                        <img className="rounded img-fluid" alt={this.props.title} src={this.props.src} />
+                    </a>
+                </div>
+            </div>
         );
     }
 }
