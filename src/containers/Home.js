4338304f4346868b0620beaa2b3d74b9c708b3e9
Brian Firestone <bfirestone2339@gmail.com>
2021-05-05 11:32:49 -0700

    Implement CollectionSelector, local state, and API call

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,43 @@
-import React, { Component } from 'react'
-import GifListContainer from './GifListContainer'
+import React, { Component } from 'react';
+import GifListContainer from './GifListContainer';
+import GifSearchContainer from './GifSearchContainer';
+import CollectionSelector from '../components/CollectionSelector';
+import { Jumbotron, Accordian } from 'react-bootstrap';
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            gifs: [],
+            active_collection: 'none'
+        }
+    }
+
+    fetchData = (query = '') => {
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC`)
+        .then(resp => resp.json())
+        .then(({data}) => {
+            this.setState({
+                gifs: data.slice(0, 10)
+            })
+        })
+    }
+
+    selectCollection = (e) => {
+        this.setState({
+            active_collection: e.target.value
+        })
+    }
+
     render() {
         return (
-            <div>
-                <GifListContainer />
+            <div>   
+                <Jumbotron fluid>
+                    <CollectionSelector collections={this.props.collections} selectCollection={this.selectCollection} activeCollection={this.state.active_collection} />
+                    <GifSearchContainer getGifs={this.fetchData} />
+                </Jumbotron>
+                
+                <GifListContainer activeCollection={this.state.active_collection} addGif={this.props.addGif} gifs={this.state.gifs} />
             </div>
         )
     }
