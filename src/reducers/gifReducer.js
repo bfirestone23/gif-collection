78f4bb23d1eee4e49be8e14e91a4e2375a318cca
Brian Firestone <bfirestone2339@gmail.com>
2021-05-05 11:34:44 -0700

    Implement collection/add and gif/add, import uuid

--- src/reducers/gifReducer.js
+++ src/reducers/gifReducer.js
@@ -1,13 +1,24 @@
-const gifReducer = (state = { collections: [{name: 'test', id: 1, gifs: []}] }, action) => {
+import uuid from 'uuid';
+
+const gifReducer = (state = { collections: [{name: 'test', id: 1, gifs: []}, {name: 'test2', id: 2, gifs: []}] }, action) => {
     switch(action.type) {
         case 'collection/add':
-            debugger;
-            break;
+            const collection = {name: action.collection, id: uuid(), gifs: []};
+            return {...state, collections: state.collections.concat(collection)};
         case 'collection/remove':
             debugger;
-            // add gif to collection
-            // remove gif from collection
             break;
+        case 'gif/add':
+
+            let newCollectionsState = state.collections.map(collection => {
+                if (collection.name === action.gifData.collection.name) {
+                    return { ...collection, gifs: collection.gifs.concat(action.gifData.gif) };
+                } else {
+                    return collection; 
+                }
+            });
+            
+            return { ...state, collections: newCollectionsState };
         default:
             return state;
     }
