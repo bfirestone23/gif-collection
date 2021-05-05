e7d3955d6dde3fe5ce68e0345e05cf7f4c1a670d
Brian Firestone <bfirestone2339@gmail.com>
2021-05-05 11:31:37 -0700

    Update to render GifSearch, move fetchData and local state to App

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,35 +1,17 @@
 import React, { Component } from 'react';
-import GifSearch from '../components/GifSearch';
-import GifList from './GifList';
+import GifList from '../components/GifList';
 
 
 export default class GifListContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            gifs: []
-        }
-    }
 
-    // componentDidMount = () => {
-    //     this.fetchData();
-    // }
-
-    fetchData = (query = '') => {
-        fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC`)
-        .then(resp => resp.json())
-        .then(({data}) => {
-            this.setState({
-                gifs: data.slice(0, 10)
-            })
-        })
+    handleClick = (gifData) => {
+        this.props.addGif(gifData)
     }
 
     render() {
         return (
             <div>
-                <GifSearch getGifs={this.fetchData} />
-                <GifList gifs={this.state.gifs} />
+                <GifList activeCollection={this.props.activeCollection} addGif={this.handleClick} gifs={this.props.gifs} />
             </div>
         )
     }
