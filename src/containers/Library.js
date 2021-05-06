3b9ac90e818c631461e63b3930fe58359a47e599
Brian Firestone <bfirestone2339@gmail.com>
2021-05-06 11:24:19 -0700

    Pass removeGif to CollectionsContainer

--- src/containers/Library.js
+++ src/containers/Library.js
@@ -4,12 +4,11 @@ import CollectionFormContainer from './CollectionFormContainer';
 
 export default class Library extends Component {
     render() {
-        console.log(this.props.collections)
         return (
             <div>
                 <h3>My Library</h3>
                 <CollectionFormContainer addCollection={this.props.addCollection} />
-                <CollectionsContainer collections={this.props.collections} />
+                <CollectionsContainer collections={this.props.collections} removeGif={this.props.removeGif}/>
             </div>
         )
     }
