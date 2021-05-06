ed576b22731102ce8555546786ffcefd493888cc
Brian Firestone <bfirestone2339@gmail.com>
2021-05-06 11:23:52 -0700

    Move activeCollection state to Redux, update passing of props to child components

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import GifListContainer from './GifListContainer';
 import GifSearchContainer from './GifSearchContainer';
 import CollectionSelector from '../components/CollectionSelector';
-import { Jumbotron, Accordian } from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            gifs: [],
-            active_collection: 'none'
+            gifs: []
         }
     }
 
@@ -17,15 +16,10 @@ export default class Home extends Component {
         fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC`)
         .then(resp => resp.json())
         .then(({data}) => {
+            console.log(data);
             this.setState({
                 gifs: data.slice(0, 10)
             })
-        })
-    }
-
-    selectCollection = (e) => {
-        this.setState({
-            active_collection: e.target.value
         })
     }
 
@@ -33,11 +27,11 @@ export default class Home extends Component {
         return (
             <div>   
                 <Jumbotron fluid>
-                    <CollectionSelector collections={this.props.collections} selectCollection={this.selectCollection} activeCollection={this.state.active_collection} />
+                    <CollectionSelector collections={this.props.collections} selectCollection={this.props.selectCollection} activeCollection={this.props.activeCollection} />
                     <GifSearchContainer getGifs={this.fetchData} />
                 </Jumbotron>
                 
-                <GifListContainer activeCollection={this.state.active_collection} addGif={this.props.addGif} gifs={this.state.gifs} />
+                <GifListContainer activeCollection={this.props.activeCollection} addGif={this.props.addGif} gifs={this.state.gifs} />
             </div>
         )
     }
