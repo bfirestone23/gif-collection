d3ff10cf67938741517491ef37d2f1e9b1b7588b
Brian Firestone <bfirestone2339@gmail.com>
2021-05-06 11:19:25 -0700

    Add collection.id to dropdown options, set defaultValue

--- src/components/CollectionSelector.js
+++ src/components/CollectionSelector.js
@@ -1,24 +1,21 @@
-import React, { Component, Link } from 'react';
+import React, { Component } from 'react';
 import { FormControl, FormGroup, Form } from 'react-bootstrap';
 
 export default class CollectionSelector extends Component {
+
+    renderCollectionOptions = () => {
+        return this.props.collections.map(collection => <option id={collection.id} key={collection.id}>{collection.name}</option>)
+    }
+
     render() {
-        // debugger;
-
-        const renderCollectionOptions = () => {
-            return this.props.collections.map(collection => <option key={collection.id}>{collection.name}</option>)
-        }
-
-        // update active_collection on Change
-
         return (
             <div>
                 <form>
                     <FormGroup >
                         <Form.Label>Select or <a href="/library">Create</a> a Collection:</Form.Label>
-                        <FormControl id="collection-select" className="m-auto w-auto" as="select" value={this.props.activeCollection} onChange={this.props.selectCollection} >
+                        <FormControl id="collection-select" className="m-auto w-auto" as="select" defaultValue="none" onChange={(e) => this.props.selectCollection(e)} >
                             <option value='none' placeholder='Select a Collection' disabled/>
-                            {renderCollectionOptions()}
+                            {this.renderCollectionOptions()}
                         </FormControl>
                     </FormGroup>
                 </form>
