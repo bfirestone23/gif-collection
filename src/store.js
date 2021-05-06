3172dd011b313f10e1fd35b95e6da33f26568b31
Brian Firestone <bfirestone2339@gmail.com>
2021-05-06 11:25:40 -0700

    Remove comments

--- src/store.js
+++ src/store.js
@@ -2,10 +2,6 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import gifReducer from './reducers/gifReducer';
 
-// const store = createStore(gifReducer, applyMiddleware(thunk))
-
-// export default store;
-
 export function configureStore() {
     return createStore(
         gifReducer,
