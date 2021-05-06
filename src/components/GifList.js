3ba945b8d92ab1be07d084141b9eff8ca14e7268
Brian Firestone <bfirestone2339@gmail.com>
2021-05-06 11:20:33 -0700

    Move renderGifs out of render method, pass gif preview prop

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
-import GifSearchResult from './GifSearchResult';
+import Gif from './Gif';
 import { Row, Col } from 'react-bootstrap';
 import uuid from 'uuid';
 
 export default class GifList extends Component {
+
+    renderGifs = () => {
+        return this.props.gifs.map(gif => {
+        return <Col key={uuid()} className="p-1" xs={4}>
+                 <Gif 
+                    activeCollection={this.props.activeCollection}
+                    addGif={this.props.addGif} 
+                    url={gif.url} 
+                    title={gif.title} 
+                    id={gif.id} 
+                    key={gif.id} 
+                    src={gif.images.fixed_width_downsampled.url}
+                    preview={gif.images.fixed_width_small_still.url} 
+                 />
+               </Col>
+        });
+    }
+
     render() {
-
-        const renderGifs = this.props.gifs.map(gif => {
-            return <Col key={uuid()} className="p-1" xs={4}>
-                     <GifSearchResult 
-                        activeCollection={this.props.activeCollection}
-                        addGif={this.props.addGif} 
-                        url={gif.url} 
-                        title={gif.title} 
-                        id={gif.id} 
-                        key={gif.id} 
-                        src={gif.images.fixed_height_downsampled.url} 
-                     />
-                   </Col>
-        })
-
         return (
             <div>
                 <Row className="align-items-center justify-content-center m-auto">
-                    {renderGifs}
+                    {this.renderGifs()}
                 </Row>
             </div>
         )
