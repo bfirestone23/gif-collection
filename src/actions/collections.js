c6911d5f6203dc10d56284106ed35fd18db8f513
Brian Firestone <bfirestone2339@gmail.com>
2021-05-06 11:17:09 -0700

    Implement selectCollection, addCollection, and getCollections

--- src/actions/collections.js
+++ src/actions/collections.js
@@ -1,7 +1,31 @@
-export const addCollection = (collection) => {
-    return { type: 'collection/add', collection }
+export const selectCollection = (e) => {
+    return { type: 'collection/select', collection: e.target.value }
 }
+
+export const addCollection = (collection) => {
+    return dispatch => {
+        fetch('http://localhost:3001/collections', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name: collection})
+        })
+            .then(response => response.json())
+            .then(collection => dispatch({ type: 'collection/add', collection}))
+    }
+}
+
 
 export const removeCollection = (collectionId) => {
     return { type: 'collection/remove', collectionId }
+}
+
+export const getCollections = () => {
+    return dispatch => {
+        fetch('http://localhost:3001/collections')
+            .then(response => response.json())
+            .then(collections => dispatch({ type: 'collections/get', collections }));
+    };
 }
