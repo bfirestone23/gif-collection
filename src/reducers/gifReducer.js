aefc23d529f82e1e98290420ac0305764230ea95
Brian Firestone <bfirestone2339@gmail.com>
2021-05-06 11:25:17 -0700

    Implement gif/remove, collection/select, collection/remove

--- src/reducers/gifReducer.js
+++ src/reducers/gifReducer.js
@@ -1,23 +1,33 @@
-import uuid from 'uuid';
-
-const gifReducer = (state = { collections: [{name: 'test', id: 1, gifs: []}, {name: 'test2', id: 2, gifs: []}] }, action) => {
+const gifReducer = (state = { collections: [], activeCollection: 'none' }, action) => {
     switch(action.type) {
+        case 'gif/remove':
+            let updatedCollectionsState = state.collections.map(collection => {
+                if (collection.id === action.gifData.collectionId) {
+                    return { ...collection, gifs: collection.gifs.filter(gif => gif.id !== action.gifData.id) };
+                } else {
+                    return collection;
+                }
+            })
+
+            return { ...state, collections: updatedCollectionsState };
+        case 'collection/select':
+            const selectedCollection = state.collections.find(collection => collection.name === action.collection)
+            return { ...state, activeCollection: selectedCollection.id };
         case 'collection/add':
-            const collection = {name: action.collection, id: uuid(), gifs: []};
-            return {...state, collections: state.collections.concat(collection)};
-        case 'collection/remove':
-            debugger;
-            break;
+            return { ...state, collections: state.collections.concat(action.collection) };
+        case 'collections/get':
+            return { ...state, collections: state.collections.concat(action.collections) };
         case 'gif/add':
+            let gifObj = { url: action.data.url, preview: action.data.preview, title: action.data.title, src: action.data.src, id: action.data.id }
 
             let newCollectionsState = state.collections.map(collection => {
-                if (collection.name === action.gifData.collection.name) {
-                    return { ...collection, gifs: collection.gifs.concat(action.gifData.gif) };
+                if (collection.id === action.data.collection.id) {
+                    return { ...collection, gifs: collection.gifs.concat(gifObj) }
                 } else {
-                    return collection; 
+                    return collection;
                 }
-            });
-            
+            })
+
             return { ...state, collections: newCollectionsState };
         default:
             return state;
