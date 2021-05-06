4f60d101565ad57f88f46d5580f3c77457ad84ef
Brian Firestone <bfirestone2339@gmail.com>
2021-05-06 11:18:13 -0700

    Implement CollectionPreview component

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,32 +1,14 @@
 import React, { Component } from 'react';
-import { Jumbotron, Carousel } from 'react-bootstrap';
-import GifCollection from './GifCollection';
+import { Jumbotron } from 'react-bootstrap';
+import CollectionPreview from './CollectionPreview';
 
 export default class Collection extends Component {
     render() {
-        const renderCarousel = () => {
-            if (this.props.collection.gifs.length > 0) {
-                return this.props.collection.gifs.map(gif => {
-                    return (<Carousel>
-                                <Carousel.Item>
-                                    <GifCollection 
-                                        activeCollection={this.props.collection.name}
-                                        url={gif.url} 
-                                        title={gif.title} 
-                                        id={gif.id} 
-                                        key={gif.id} 
-                                        src={gif.src} 
-                                    />
-                                </Carousel.Item>
-                            </Carousel>);
-            }
-        )}};
-
         return (
             <Jumbotron className="rounded m-2 w-auto">
                 <h1>{this.props.collection.name}</h1>
                 <p>The name of this collection is {this.props.collection.name}</p>
-                {renderCarousel()}
+                <CollectionPreview collection={this.props.collection} removeGif={this.props.removeGif} />
             </Jumbotron>
         )
     }
