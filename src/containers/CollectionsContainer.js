8fad1d2f6b52bbc82e577bc1f408f27bf3776cd3
Brian Firestone <bfirestone2339@gmail.com>
2021-05-06 11:22:46 -0700

    Move renderCollections() out of render method

--- src/containers/CollectionsContainer.js
+++ src/containers/CollectionsContainer.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react'
 import Collection from '../components/Collection';
 
 export default class CollectionsContainer extends Component {
+    
+    renderCollections = () => {
+        return this.props.collections.map(collection => <Collection key={collection.id} collection={collection} removeGif={this.props.removeGif}/>)
+    }
+
 
     render() {
-
-        const renderCollections = () => {
-            return this.props.collections.map(collection => <Collection key={collection.id} collection={collection} />)
-        }
-
         return (
             <div className="">
-                {renderCollections()}
+                {this.renderCollections()}
             </div>
         )
     }
