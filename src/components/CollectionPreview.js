3f974eff746eea5a612e1d156ff180e98526d98a
Brian Firestone <bfirestone2339@gmail.com>
2021-05-07 12:21:50 -0700

    Implement spinner and adjust justifyContent styling

--- src/components/CollectionPreview.js
+++ src/components/CollectionPreview.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 import Gif from './Gif';
+import { Spinner } from 'react-bootstrap';
 
 
 export default class CollectionPreview extends Component {
-
     renderCollection = () => {
-        return this.props.collection.gifs.slice(0, 10).map(gif => {
-            return <Gif 
-                        collectionId={this.props.collection.id}
-                        url={gif.url} 
-                        title={gif.title} 
-                        id={gif.id} 
-                        key={gif.id} 
-                        src={gif.preview}
-                        removeGif={this.props.removeGif}
-                    />
-        })
+        if (this.props.isLoading === true) {
+            return <Spinner className='m-5' animation='border' variant='primary' />
+        } else {
+            return this.props.collection.gifs.slice(0, 10).map(gif => {
+                return <Gif 
+                            collectionId={this.props.collection.id}
+                            url={gif.url} 
+                            title={gif.title} 
+                            id={gif.id} 
+                            key={gif.id} 
+                            src={gif.preview}
+                            removeGif={this.props.removeGif}
+                        />
+            })
+        }
     }
-
 
     render() {
         return (
-            <div style={{width: '75%', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around'}}className='container-fluid'>
+            <div style={{width: '75%', display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}className='container-fluid'>
                 {this.renderCollection()}
             </div>
         )
     }
 }
+
+
