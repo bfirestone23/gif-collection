ab8917c5a3fa73542891cb7bbd33aa8e3121814c
Brian Firestone <bfirestone2339@gmail.com>
2021-05-07 12:59:23 -0700

    Implement pagination, gif/collection loading, gif search cases

--- src/reducers/gifReducer.js
+++ src/reducers/gifReducer.js
@@ -1,22 +1,25 @@
-const gifReducer = (state = { collections: [], activeCollection: 'none' }, action) => {
-    switch(action.type) {
-        case 'gif/remove':
-            let updatedCollectionsState = state.collections.map(collection => {
-                if (collection.id === action.gifData.collectionId) {
-                    return { ...collection, gifs: collection.gifs.filter(gif => gif.id !== action.gifData.id) };
-                } else {
-                    return collection;
-                }
-            })
+const gifReducer = (state = 
+    { searchResults: [], 
+      collections: [], 
+      activeCollection: 'none', 
+      isLoading: false, 
+      isError: false, 
+      currentPage: 1, 
+      itemsPerPage: 15 
+    }, action) => {
 
-            return { ...state, collections: updatedCollectionsState };
+    switch(action.type) {
         case 'collection/select':
             const selectedCollection = state.collections.find(collection => collection.name === action.collection)
             return { ...state, activeCollection: selectedCollection.id };
         case 'collection/add':
             return { ...state, collections: state.collections.concat(action.collection) };
+        case 'collection/remove':
+            return { ...state, collections: state.collections.filter(c => c.id !== action.collectionId) }
+        case 'collections/loading':
+            return { ...state, isLoading: true };
         case 'collections/get':
-            return { ...state, collections: state.collections.concat(action.collections) };
+            return { ...state, collections: [...state.collections.concat(action.collections)], isLoading: false };
         case 'gif/add':
             let gifObj = { url: action.data.url, preview: action.data.preview, title: action.data.title, src: action.data.src, id: action.data.id }
 
@@ -29,6 +32,22 @@ const gifReducer = (state = { collections: [], activeCollection: 'none' }, actio
             })
 
             return { ...state, collections: newCollectionsState };
+        case 'gif/remove':
+            let updatedCollectionsState = state.collections.map(collection => {
+                if (collection.id === action.gifData.collectionId) {
+                    return { ...collection, gifs: collection.gifs.filter(gif => gif.id !== action.gifData.id) };
+                } else {
+                    return collection;
+                }
+            })
+
+            return { ...state, collections: updatedCollectionsState };
+        case 'gifs/loading':
+            return { ...state, isLoading: true };
+        case 'gif/search':
+            return { ...state, searchResults: action.gifs, isLoading: false, isError: false }
+        case 'gifs/updatePage':
+            return { ...state, currentPage: action.pageNumber };
         default:
             return state;
     }
