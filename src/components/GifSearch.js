30ac63612bacb576b33cbd2eefbef61b5017c584
Brian Firestone <bfirestone2339@gmail.com>
2021-05-07 12:27:15 -0700

    Move state and event handling to parent, convert to function

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -1,38 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Button, FormGroup, FormControl } from 'react-bootstrap';
 
-export default class GifSearch extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            query: ''
-        }
-    }
-
-    handleChange = (event) => {
-        this.setState({
-            query: event.target.value
-        })
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault()
-        this.props.getGifs(this.state.query)
-        this.setState({
-            query: ''
-        }) 
-    }
-
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <FormGroup>
-                        <FormControl className="w-25 m-auto" placeholder="Enter your search" name="query" value={this.state.query} onChange={this.handleChange} type="text" />
-                        <Button className="submit m-3" type="submit">Search Giphy</Button>
-                    </FormGroup>
-                </form>
-            </div>
-        )
-    }
+export default function GifSearch(props) {
+    return (
+        <div>
+            <form onSubmit={props.handleSubmit}>
+                <FormGroup>
+                    <FormControl 
+                        className="w-50 m-auto" 
+                        placeholder="Enter your search" 
+                        name="query" 
+                        value={props.query} 
+                        onChange={props.handleChange} 
+                        type="text" 
+                    />
+                    <Button 
+                        className="submit m-3" 
+                        type="submit">
+                        Search Giphy
+                    </Button>
+                </FormGroup>
+            </form>
+        </div>
+    )
 }
