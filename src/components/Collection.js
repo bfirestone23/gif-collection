bfa1a7af6b6567bedca06836966d5a6b0b333939
Brian Firestone <bfirestone2339@gmail.com>
2021-05-07 12:17:57 -0700

    Implement renderPreview method, removeCollection button

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Button } from 'react-bootstrap';
 import CollectionPreview from './CollectionPreview';
 
 export default class Collection extends Component {
+
+    renderPreview = () => {
+        if (this.props.collection.gifs.length === 0) {
+            return <div>No gifs have been added to this collection.</div>
+        } else {
+            return <CollectionPreview collection={this.props.collection} removeGif={this.props.removeGif} />
+        }
+    }
+
     render() {
         return (
-            <Jumbotron className="rounded m-2 w-auto">
-                <h1>{this.props.collection.name}</h1>
-                <p>The name of this collection is {this.props.collection.name}</p>
-                <CollectionPreview collection={this.props.collection} removeGif={this.props.removeGif} />
+            <Jumbotron style={{position: 'relative'}} className="rounded m-2 w-auto">
+                <Button style={{position: 'absolute', top: '.5rem', left: '.5rem'}} onClick={() => this.props.removeCollection(this.props.collection.id)}>X</Button>
+                <h3 className='m-2' >{this.props.collection.name}</h3>
+                {this.renderPreview()}
             </Jumbotron>
         )
     }
