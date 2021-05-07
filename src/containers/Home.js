518f7dadd82e8fe69961e5dd4cc5b514bb1f6050
Brian Firestone <bfirestone2339@gmail.com>
2021-05-07 12:46:08 -0700

    Remove local state and fetchData function, moved to Redux

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,34 +4,23 @@ import GifSearchContainer from './GifSearchContainer';
 import CollectionSelector from '../components/CollectionSelector';
 import { Jumbotron } from 'react-bootstrap';
 
+
 export default class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            gifs: []
-        }
-    }
-
-    fetchData = (query = '') => {
-        fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC`)
-        .then(resp => resp.json())
-        .then(({data}) => {
-            console.log(data);
-            this.setState({
-                gifs: data.slice(0, 10)
-            })
-        })
-    }
-
     render() {
         return (
-            <div>   
-                <Jumbotron fluid>
-                    <CollectionSelector collections={this.props.collections} selectCollection={this.props.selectCollection} activeCollection={this.props.activeCollection} />
+            <div className='App'>   
+                <Jumbotron fluid className='rounded w-75 mx-auto my-2'>
+                    <CollectionSelector 
+                        collections={this.props.collections} 
+                        selectCollection={this.props.selectCollection} 
+                        activeCollection={this.props.activeCollection} 
+                    />
                     <GifSearchContainer getGifs={this.fetchData} />
                 </Jumbotron>
-                
-                <GifListContainer activeCollection={this.props.activeCollection} addGif={this.props.addGif} gifs={this.state.gifs} />
+                <GifListContainer 
+                    activeCollection={this.props.activeCollection} 
+                    addGif={this.props.addGif} 
+                />
             </div>
         )
     }
