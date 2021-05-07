825452f2c80d60d1353eff824a527250bf29752a
Brian Firestone <bfirestone2339@gmail.com>
2021-05-07 12:45:17 -0700

    Implement local state and pass event handlers to GifSearch, connect to Redux for searchGifs function

--- src/containers/GifSearchContainer.js
+++ src/containers/GifSearchContainer.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 import GifSearch from '../components/GifSearch';
+import { connect } from 'react-redux';
+import { searchGifs } from '../actions/gifs';
 
-export default class GifSearchContainer extends Component {
+class GifSearchContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: '',
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            query: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.searchGifs(this.state.query)
+        this.setState({
+            query: ''
+        }) 
+    }
+
     render() {
         return (
             <div>
-                <GifSearch getGifs={this.props.getGifs} />
+                <GifSearch 
+                    getGifs={this.props.getGifs}
+                    handleChange={this.handleChange}
+                    handleSubmit={this.handleSubmit}
+                    query={this.state.query} 
+                />
             </div>
         )
     }
 }
+
+export default connect(null, { searchGifs })(GifSearchContainer);
