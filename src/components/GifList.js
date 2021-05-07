497603fca110b5f9c1131ea5bcc1b76e0bf7ef64
Brian Firestone <bfirestone2339@gmail.com>
2021-05-07 12:25:54 -0700

    Implement loading spinner and adjust styling, remove Row and Col components

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react';
 import Gif from './Gif';
-import { Row, Col } from 'react-bootstrap';
-import uuid from 'uuid';
+import { Spinner } from 'react-bootstrap';
+
 
 export default class GifList extends Component {
 
     renderGifs = () => {
-        return this.props.gifs.map(gif => {
-        return <Col key={uuid()} className="p-1" xs={4}>
-                 <Gif 
-                    activeCollection={this.props.activeCollection}
-                    addGif={this.props.addGif} 
-                    url={gif.url} 
-                    title={gif.title} 
-                    id={gif.id} 
-                    key={gif.id} 
-                    src={gif.images.fixed_width_downsampled.url}
-                    preview={gif.images.fixed_width_small_still.url} 
-                 />
-               </Col>
-        });
+        if (this.props.isLoading) {
+            return <Spinner className='m-5' animation='border' variant='primary' />
+        } else {
+            return this.props.gifs.map(gif => {
+                // debugger;
+                return (
+                        <Gif 
+                            activeCollection={this.props.activeCollection}
+                            addGif={this.props.addGif} 
+                            url={gif.url} 
+                            title={gif.title} 
+                            id={gif.id} 
+                            key={gif.id} 
+                            src={gif.images.fixed_width_downsampled.url}
+                            preview={gif.images.fixed_width_small.url} 
+                        />
+                    )
+                });
+        }
     }
 
     render() {
         return (
-            <div>
-                <Row className="align-items-center justify-content-center m-auto">
-                    {this.renderGifs()}
-                </Row>
+            <div style={{display: 'flex', flexWrap: 'wrap'}} className="justify-content-center align-items-center m-auto">
+                {this.renderGifs()}
             </div>
         )
     }
