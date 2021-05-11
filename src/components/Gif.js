b328862b0f051d3bd91c85400e64f24c37a1bcf3
Brian Firestone <bfirestone2339@gmail.com>
2021-05-11 09:27:36 -0700

    Add onLoad attribute to img, create imageLoaded state + handleLoad function, add imageLoaded conditional to renderButton()

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -3,13 +3,22 @@ import AddGifButton from './AddGIfButton';
 import RemoveGifButton from './RemoveGifButton';
 
 export default class Gif extends Component {
+    state = {
+        imageLoaded: false
+    }
 
     renderButton = () => {
-        if (Object.keys(this.props).includes('addGif')) {
+        if (Object.keys(this.props).includes('addGif') && this.state.imageLoaded) {
             return <AddGifButton addGif={this.props.addGif} gifData={this.props} />
-        } else if (Object.keys(this.props).includes('removeGif')) {
+        } else if (Object.keys(this.props).includes('removeGif') && this.state.imageLoaded) {
             return <RemoveGifButton removeGif={this.props.removeGif} gifData={this.props} />
         }
+    }
+
+    handleLoad = () => {
+        this.setState({
+            imageLoaded: true
+        })
     }
 
     render() {
@@ -17,7 +26,7 @@ export default class Gif extends Component {
             <div>
                 {this.renderButton()}
                 <a rel='noreferrer' target='_blank' href={this.props.url}>
-                    <img className='gif rounded img-fluid m-1' alt={this.props.title} src={this.props.src} />
+                    <img onLoad={this.handleLoad} className='gif rounded img-fluid m-1' alt={this.props.title} src={this.props.src} />
                 </a>
             </div>
         );
