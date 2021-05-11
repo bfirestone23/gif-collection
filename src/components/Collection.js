d20cd979030731ccdfd3809d7a4b2851474c71de
Brian Firestone <bfirestone2339@gmail.com>
2021-05-11 11:40:25 -0700

    Refactor to functional component, move renderPreview() to parent

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,31 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
-import CollectionPreview from './CollectionPreview';
 
-export default class Collection extends Component {
-
-    renderPreview = () => {
-        if (this.props.collection.gifs.length === 0) {
-            return <div>No gifs have been added to this collection.</div>
-        } else {
-            return <CollectionPreview collection={this.props.collection} removeGif={this.props.removeGif} />
-        }
-    }
-
-    render() {
-        return (
-                <Jumbotron className="position-relative rounded m-2 w-auto">
-                    <Button 
-                        variant='danger' 
-                        size='sm' 
-                        style={{top: '.5rem', left: '.5rem'}} 
-                        className='position-absolute py-0 m-1 w-auto align-text-center'
-                        onClick={() => this.props.removeCollection(this.props.collection.id)}>
-                            x
-                    </Button>
-                    <h3 className='m-2' >{this.props.collection.name}</h3>
-                    {this.renderPreview()}
-                </Jumbotron>            
-        )
-    }
+export default function Collection(props) {
+    return (
+        <Jumbotron className="position-relative rounded m-2 w-auto">
+            <Button 
+                variant='danger' 
+                size='sm' 
+                style={{top: '.5rem', left: '.5rem'}} 
+                className='position-absolute py-0 m-1 w-auto align-text-center'
+                onClick={() => props.removeCollection(props.collection.id)}>
+                    x
+            </Button>
+            <h3 className='m-2' >{props.collection.name}</h3>
+            {props.renderPreview()}
+        </Jumbotron>
+    )
 };
