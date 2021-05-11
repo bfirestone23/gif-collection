b8def6372608c49cf2f65ab8aed94f0a67d5a7aa
Brian Firestone <bfirestone2339@gmail.com>
2021-05-11 09:24:24 -0700

    Separate classes by line for readability

--- src/components/RemoveGifButton.js
+++ src/components/RemoveGifButton.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { Button } from 'react-bootstrap';
 
-export default function RemoveGifButton(props) {
-    return (
-        <Button 
-            size='sm' 
-            variant='danger'
-            className='remove-gif my-button rounded p-1 m-1 w-auto' 
-            id={props.gifData.id} 
-            onClick={() => props.removeGif(props.gifData)}>
-            -
-        </Button>
-    )
+export default class RemoveGifButton extends Component {
+    render() {
+        return (
+            <Button
+                size='sm' 
+                variant='danger'
+                className='
+                            remove-gif 
+                            rounded 
+                            position-absolute 
+                            py-0 
+                            m-1 
+                            w-auto 
+                            align-text-center' 
+
+                id={this.props.gifData.id} 
+                onClick={() => this.props.removeGif(this.props.gifData)}>
+                -
+            </Button>
+        )
+    }
 }
+
+
