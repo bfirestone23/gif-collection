e8d2a00cf67823d1e94cb17ed6b4c1711f7503cf
Brian Firestone <bfirestone2339@gmail.com>
2021-05-11 09:31:19 -0700

    Fix capitalization in file name

--- src/components/AddGIfButton.js
+++ src/components/AddGIfButton.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { Button } from 'react-bootstrap';
 
-export default function AddGifButton(props) {
-    return (
-        <Button 
-            size='sm' 
-            variant='success'
-            className='add-gif my-button rounded p-2 m-1 w-auto' 
-            id={props.gifData.id} 
-            onClick={() => props.addGif(props.gifData)}>
-            +
-        </Button>
-    );
-};
+export default class AddGifButton extends Component {
+    render() {
+        return (
+            <Button
+                size='sm' 
+                variant='success'
+                className='
+                            add-gif 
+                            rounded 
+                            position-absolute 
+                            py-0 
+                            m-1 
+                            w-auto 
+                            align-text-center' 
+                            
+                id={this.props.gifData.id} 
+                onClick={() => this.props.addGif(this.props.gifData)}>
+                +
+            </Button>
+        )
+    }
+}
