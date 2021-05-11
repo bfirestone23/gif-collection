7a01a912f044306ebf9cc2dc3f444ca13663b882
Brian Firestone <bfirestone2339@gmail.com>
2021-05-11 11:43:01 -0700

    Move renderCollections() to parent as renderCollectionContainers(), convert to functional component

--- src/containers/CollectionsContainer.js
+++ src/containers/CollectionsContainer.js
@@ -1,22 +1,10 @@
-import React, { Component } from 'react'
-import Collection from '../components/Collection';
+import React from 'react';
 
-export default class CollectionsContainer extends Component {
-
-    renderCollections = () => {
-        return this.props.collections.map(collection => <Collection 
-                                                            key={collection.id} 
-                                                            collection={collection} 
-                                                            removeCollection={this.props.removeCollection} 
-                                                            removeGif={this.props.removeGif}
-                                                        />)
-    }
-
-    render() {
-        return (
-            <div className="w-50 m-auto">
-                {this.renderCollections()}
-            </div>
-        )
-    }
+export default function CollectionsContainer(props) {
+    
+    return (
+        <div className="w-50 m-auto">
+            {props.renderCollectionContainers()}
+        </div>
+    )
 }
