bb11411e708df4e94bdf9bcd84ff6682339ab910
Brian Firestone <bfirestone2339@gmail.com>
2021-05-11 11:41:17 -0700

    Refactor to functional component and implement renderCollectionContainers from child component

--- src/containers/Library.js
+++ src/containers/Library.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CollectionsContainer from './CollectionsContainer';
 import CollectionFormContainer from './CollectionFormContainer';
+import CollectionContainer from './CollectionContainer';
 
-export default class Library extends Component {
-    render() {
-        return (
-            <div className='Library'>
-                <CollectionFormContainer addCollection={this.props.addCollection} />
-                <CollectionsContainer 
-                    collections={this.props.collections} 
-                    removeCollection={this.props.removeCollection} 
-                    removeGif={this.props.removeGif}
-                />
-            </div>
-        )
+export default function Library(props) {
+
+    const renderCollectionContainers = () => {
+        return props.collections.map(collection => <CollectionContainer 
+                                                            key={collection.id} 
+                                                            collection={collection} 
+                                                            removeCollection={props.removeCollection} 
+                                                            removeGif={props.removeGif}
+                                                        />)
     }
+
+    return (
+        <div className='Library'>
+            <CollectionFormContainer addCollection={props.addCollection} />
+            <CollectionsContainer 
+                renderCollectionContainers={renderCollectionContainers}
+                collections={props.collections} 
+                removeCollection={props.removeCollection} 
+                removeGif={props.removeGif}
+            />
+        </div>
+    )
 }
 
