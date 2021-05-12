4f5f7dbdcffcf65f2a0af70f667ebe4bb0af806e
Brian Firestone <bfirestone2339@gmail.com>
2021-05-12 11:05:45 -0700

    Implement displayLogin, specify reducers in mapStateToProps

--- src/App.js
+++ src/App.js
@@ -3,47 +3,84 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Switch
+  Switch,
+  Redirect
 } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './containers/Home';
 import Library from './containers/Library';
+import Login from './components/Login';
+import { Button } from 'react-bootstrap';
 
 import { connect } from 'react-redux';
 import { selectCollection, addCollection, removeCollection, getCollections } from './actions/collections';
 import { addGif, removeGif } from './actions/gifs';
+import { setStatus, logout } from './actions/users';
 
 class App extends Component {
 
   componentDidMount() {
     this.props.getCollections();
+    this.props.setStatus();
+  }
+
+  displayLogin = () => {
+    if (this.props.status === 'idle') {
+      return (
+        <div>
+          <Redirect to='/login' />
+          <Route path='/login' component={Login} />
+        </div>
+        
+      )
+    }
+    if (this.props.status === 'pending') {
+      return <div>Loading...</div>
+    }
+    if (this.props.status === 'resolved') {
+      return (
+        <div>
+          <Redirect to='/home' />
+          <NavBar />
+          <Route exact path='/home'>
+            <div className='mb-5'>
+              <h4 style={{color: 'white'}}>Welcome, {this.props.username}!</h4><Button size='sm' onClick={this.props.logout}>Log Out</Button>
+            </div>
+            <Home 
+              addGif={this.props.addGif} 
+              activeCollection={this.props.activeCollection} 
+              selectCollection={this.props.selectCollection} 
+              collections={this.props.collections}
+            />
+          </Route>
+          <Route path='/collections'>
+            <Library 
+              collections={this.props.collections} 
+              getCollections={this.props.getCollections}
+              addCollection={this.props.addCollection} 
+              removeCollection={this.props.removeCollection} 
+              removeGif={this.props.removeGif} 
+            />
+          </Route>
+        </div>
+      )
+    }
+    if (this.props.status === 'rejected') {
+      return (
+        <div>
+          <div>Oh no, you aren't logged in.</div>
+        </div>
+      )
+    }
   }
 
   render() {
     return (
       <Router>
-        <div className="App m-auto p-1">
-          <NavBar />
+        <div className='App m-auto p-1'>
           <Switch>
-            <Route exact path="/">
-              <Home 
-                addGif={this.props.addGif} 
-                activeCollection={this.props.activeCollection} 
-                selectCollection={this.props.selectCollection} 
-                collections={this.props.collections}
-              />
-            </Route>
-            <Route path="/collections">
-              <Library 
-                collections={this.props.collections} 
-                getCollections={this.props.getCollections}
-                addCollection={this.props.addCollection} 
-                removeCollection={this.props.removeCollection} 
-                removeGif={this.props.removeGif} 
-              />
-            </Route>
+            {this.displayLogin()}
           </Switch>
-          
         </div>
       </Router>
       
@@ -54,10 +91,12 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return { 
-    collections: state.collections,
-    activeCollection: state.activeCollection 
+    collections: state.collection.collections,
+    activeCollection: state.collection.activeCollection,
+    status: state.user.status,
+    username: state.user.username 
   }
 }
 
 
-export default connect(mapStateToProps, { selectCollection, addCollection, removeCollection, getCollections, addGif, removeGif })(App);
+export default connect(mapStateToProps, { setStatus, logout, selectCollection, addCollection, removeCollection, getCollections, addGif, removeGif })(App);
