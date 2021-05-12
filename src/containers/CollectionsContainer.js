3d071bae3316ca2890c8a9f89e4512f96ad051ca
Brian Firestone <bfirestone2339@gmail.com>
2021-05-12 10:58:45 -0700

    Standardize quotes

--- src/containers/CollectionsContainer.js
+++ src/containers/CollectionsContainer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function CollectionsContainer(props) {
     
     return (
-        <div className="w-50 m-auto">
+        <div className='w-50 m-auto'>
             {props.renderCollectionContainers()}
         </div>
     )
