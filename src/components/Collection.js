4e1b5683ee50ee4949dd175d21708471f0f82c7d
Brian Firestone <bfirestone2339@gmail.com>
2021-05-14 14:06:34 -0700

    Remove renderButton and insert Button component directly

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { Jumbotron, Button } from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 
 export default function Collection(props) {
     return (
-        <Jumbotron className='position-relative rounded m-2 w-auto'>
-            <Button 
-                variant='danger' 
-                size='sm' 
-                style={{top: '.5rem', left: '.5rem'}} 
-                className='position-absolute py-0 m-1 w-auto align-text-center'
-                onClick={() => props.removeCollection(props.collection.id)}>
-                    x
-            </Button>
-            <h3 className='m-2' >{props.collection.name} by {props.collection.user.username}</h3>
+        <Jumbotron className='position-relative rounded mx-auto mb-3 w-75'>
+            {props.renderButton()}
+            <h3>{props.collection.name}</h3><h6> by {props.collection.user.username}</h6> 
             {props.renderPreview()}
         </Jumbotron>
     )
