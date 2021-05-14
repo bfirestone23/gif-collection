f8233ec9d9b27ba17e79ff7e1adf1818a793cd1d
Brian Firestone <bfirestone2339@gmail.com>
2021-05-14 13:57:52 -0700

    Convert to functional component

--- src/components/RemoveGifButton.js
+++ src/components/RemoveGifButton.js
@@ -1,27 +1,25 @@
-import React, { Component} from 'react';
+import React from 'react';
 import { Button } from 'react-bootstrap';
 
-export default class RemoveGifButton extends Component {
-    render() {
-        return (
-            <Button
-                size='sm' 
-                variant='danger'
-                className='
-                            remove-gif 
-                            rounded 
-                            position-absolute 
-                            py-0 
-                            m-1 
-                            w-auto 
-                            align-text-center' 
+export default function RemoveGifButton(props) {
+    return (
+        <Button
+            size='sm' 
+            variant='danger'
+            className='
+                        remove-gif 
+                        rounded 
+                        position-absolute 
+                        py-0 
+                        m-1 
+                        w-auto 
+                        align-text-center' 
 
-                id={this.props.gifData.id} 
-                onClick={() => this.props.removeGif(this.props.gifData)}>
-                -
-            </Button>
-        )
-    }
+            id={props.gifData.id} 
+            onClick={() => props.removeGif(props.gifData)}>
+            -
+        </Button>
+    )
 }
 
 
