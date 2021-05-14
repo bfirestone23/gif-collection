140566a8a9d7c156b9bc76d831549305942f3651
Brian Firestone <bfirestone2339@gmail.com>
2021-05-14 13:56:56 -0700

    Convert to class component, implement renderButton and connect to Redux

--- src/containers/CollectionContainer.js
+++ src/containers/CollectionContainer.js
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
 import CollectionPreview from '../components/CollectionPreview';
 import Collection from '../components/Collection';
 
-export default function CollectionContainer(props) {
+class CollectionContainer extends Component {
 
-    const renderPreview = () => {
-        if (props.collection.gifs.length === 0) {
+    renderPreview = () => {
+        if (this.props.collection.gifs.length === 0) {
             return <div>No gifs have been added to this collection.</div>
         } else {
             return <CollectionPreview 
-                    collection={props.collection} 
-                    removeGif={props.removeGif} 
+                    collection={this.props.collection} 
+                    removeGif={this.props.removeGif} 
                    />
         }
     }
 
-    return (
-        <div>
-            <Collection 
-                renderPreview={renderPreview}
-                removeCollection={props.removeCollection} 
-                collection={props.collection}
-            />
-        </div>
-    )
+    renderButton = () => {
+        if (this.props.collection.user.username === this.props.currentUser) {
+            return (
+                <Button 
+                variant='danger' 
+                size='sm' 
+                style={{top: '.5rem', left: '.5rem'}} 
+                className='position-absolute py-0 m-1 w-auto align-text-center'
+                onClick={() => this.props.removeCollection(this.props.collection.id)}>
+                    X
+                </Button>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Collection 
+                    renderPreview={this.renderPreview}
+                    renderButton={this.renderButton}
+                    removeCollection={this.props.removeCollection} 
+                    collection={this.props.collection}
+                />
+            </div>
+        )
+    }
 }
+
+const mapStateToProps = state => {
+    return {
+        currentUser: state.user.username
+    }
+}
+
+export default connect(mapStateToProps)(CollectionContainer);
