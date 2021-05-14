dbd87a5a405766c08073dd1c70b70dffc91cfd2b
Brian Firestone <bfirestone2339@gmail.com>
2021-05-14 14:05:58 -0700

    Convert to functional component

--- src/components/CollectionForm.js
+++ src/components/CollectionForm.js
@@ -1,48 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FormGroup, FormControl, Button, Jumbotron } from 'react-bootstrap';
-import { connect } from 'react-redux';
 
-class CollectionForm extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            name: ''
-        }
-    }
-
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        let collection = { name: this.state.name, user: this.props.user }
-        this.props.addCollection(collection)
-        this.setState({
-            name: ''
-        })
-    }
-
-    render() {
-        return (
-            <Jumbotron className='m-auto d-flex align-content-between flex-wrap' style={{width: '25rem', height: '13rem'}}>
-                <form className='m-auto' onSubmit={this.handleSubmit}>
-                    <FormGroup>
-                        <FormControl className='m-auto' placeholder='New Collection' name='name' value={this.state.name} onChange={this.handleChange} type='text' />
-                        <Button className='submit m-3' type='submit'>Create</Button>
-                    </FormGroup>
-                </form>
-            </Jumbotron>
-        )
-    }
+export default function CollectionForm(props) {
+    return (
+        <Jumbotron className='my-5 mx-auto d-flex align-content-between flex-wrap' style={{width: '25rem', height: '13rem'}}>
+            <form className='m-auto' onSubmit={props.handleSubmit}>
+                <FormGroup>
+                    <FormControl 
+                        type='text'
+                        className='m-auto' 
+                        placeholder='New Collection' 
+                        name='name' 
+                        value={props.collectionName} 
+                        onChange={props.handleChange}
+                    />
+                    <Button className='submit m-3' type='submit'>Create</Button>
+                </FormGroup>
+            </form>
+        </Jumbotron>
+    )
 }
-
-const mapStateToProps = state => {
-    return {
-        user: state.user.username
-    }
-}
-
-export default connect(mapStateToProps)(CollectionForm);
