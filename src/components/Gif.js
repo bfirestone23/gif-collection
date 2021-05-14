a46701dd2aac5bb9a9cf1718bb3732bede84706e
Brian Firestone <bfirestone2339@gmail.com>
2021-05-14 14:02:43 -0700

    Convert to functional component and move render and event handler functions to parent component

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,34 +1,17 @@
-import React, { Component } from 'react';
-import AddGifButton from './AddGifButton';
-import RemoveGifButton from './RemoveGifButton';
+import React from 'react';
 
-export default class Gif extends Component {
-    state = {
-        imageLoaded: false
-    }
-
-    renderButton = () => {
-        if (Object.keys(this.props).includes('addGif') && this.state.imageLoaded) {
-            return <AddGifButton addGif={this.props.addGif} gifData={this.props} />
-        } else if (Object.keys(this.props).includes('removeGif') && this.state.imageLoaded) {
-            return <RemoveGifButton removeGif={this.props.removeGif} gifData={this.props} />
-        }
-    }
-
-    handleLoad = () => {
-        this.setState({
-            imageLoaded: true
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                {this.renderButton()}
-                <a rel='noreferrer' target='_blank' href={this.props.url}>
-                    <img onLoad={this.handleLoad} className='gif rounded img-fluid m-1' alt={this.props.title} src={this.props.src} />
-                </a>
-            </div>
-        );
-    }
+export default function Gif(props) {
+    return (
+        <div>
+            {props.renderButton()}
+            <a rel='noreferrer' target='_blank' href={props.url}>
+                <img 
+                    onLoad={props.handleLoad} 
+                    className='gif rounded img-fluid m-1' 
+                    alt={props.title} 
+                    src={props.src} 
+                />
+            </a>
+        </div>
+    );
 }
