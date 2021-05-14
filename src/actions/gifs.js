d8184ab2f5cbbb1797249f7cc972bcbbd9361870
Brian Firestone <bfirestone2339@gmail.com>
2021-05-14 14:07:28 -0700

    Move addGif to collections actions

--- src/actions/gifs.js
+++ src/actions/gifs.js
@@ -9,46 +9,6 @@ export const searchGifs = (query = '') => {
     };
 };
 
-export const addGif = (gifData) => {
-    return dispatch => {
-        fetch(`http://localhost:3001/collections/${gifData.activeCollection}/gifs`, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(
-                {
-                    src: gifData.src, 
-                    url: gifData.url, 
-                    preview: gifData.preview, 
-                    title: gifData.title, 
-                    collection_id: gifData.activeCollection
-                })
-        })
-            .then(response => response.json())
-            .then(data => dispatch({ type: 'add', data }))
-    }
-}
-
-export const removeGif = (gifData) => {
-    return dispatch => {
-        dispatch({ type: 'remove', gifData })
-        fetch(`http://localhost:3001/collections/${gifData.collectionId}/gifs/${gifData.id}`, {
-            method: 'DELETE',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                gif: {
-                    id: gifData.id
-                }
-            })
-        })
-    }
-}
-
 export const updateCurrentPage = pageNumber => {
     return dispatch => {
         dispatch({ type: 'updatePage', pageNumber })
