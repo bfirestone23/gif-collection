16baeac99fd035cc2ed574514948b4df155d9ff6
Brian Firestone <bfirestone2339@gmail.com>
2021-05-14 14:08:03 -0700

    Implement add/removeGif actions

--- src/actions/collections.js
+++ src/actions/collections.js
@@ -39,3 +39,43 @@ export const getCollections = () => {
             .then(collections => dispatch({ type: 'get', collections }));
     };
 }
+
+export const addGif = (gifData) => {
+    return dispatch => {
+        fetch(`http://localhost:3001/collections/${gifData.activeCollection.id}/gifs`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    src: gifData.src, 
+                    url: gifData.url, 
+                    preview: gifData.preview, 
+                    title: gifData.title, 
+                    collection_id: gifData.activeCollection.id
+                })
+        })
+            .then(response => response.json())
+            .then(data => dispatch({ type: 'gif/add', data }))
+    }
+}
+
+export const removeGif = (gifData) => {
+    return dispatch => {
+        dispatch({ type: 'gif/remove', gifData })
+        fetch(`http://localhost:3001/collections/${gifData.collectionId}/gifs/${gifData.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                gif: {
+                    id: gifData.id
+                }
+            })
+        })
+    }
+}
