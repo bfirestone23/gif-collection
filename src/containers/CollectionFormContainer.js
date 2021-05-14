8f74add77b7f2f050cb2600b3b2439a8442c374d
Brian Firestone <bfirestone2339@gmail.com>
2021-05-14 13:55:56 -0700

    Implement state and event handlers from CollectionForm component

--- src/containers/CollectionFormContainer.js
+++ src/containers/CollectionFormContainer.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CollectionForm from '../components/CollectionForm';
 
-export default function CollectionFormContainer(props) {
-    return (
-        <div>
-            <CollectionForm addCollection={props.addCollection} />
-        </div>
-    )
+class CollectionFormContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        let collection = { name: this.state.name, user: this.props.user }
+        this.props.addCollection(collection)
+        this.setState({
+            name: ''
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <CollectionForm 
+                    collectionName={this.state.name}
+                    handleSubmit={this.handleSubmit}
+                    handleChange={this.handleChange} 
+                />
+            </div>
+        )
+    }   
 }
+
+const mapStateToProps = state => {
+    return {
+        user: state.user.username
+    }
+}
+
+export default connect(mapStateToProps)(CollectionFormContainer);
