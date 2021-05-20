f573f818ae6949e6137921abd2726b4c501b7675
Brian Firestone <bfirestone2339@gmail.com>
2021-05-20 11:41:40 -0700

    Convert onClick function to callback, remove import of likeCollection and move to parent component

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { likeCollection } from '../actions/collections';
 
-class Likes extends Component {
+export default class Likes extends Component {
 
     render() {
         return (
             <div>
                 Likes: {this.props.likes}
-                <button onClick={this.props.likeCollection(this.props.collectionId, this.props.likes)}>Like</button>
+                <button onClick={() => this.props.likeCollection(this.props.collectionId, this.props.likes)}>Like</button>
             </div>
         )
     }
 }
-
-export default connect(null, { likeCollection })(Likes);
