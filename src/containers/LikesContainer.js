bd3f03ce97b71de66b75ec6e749b3a87c90ee523
Brian Firestone <bfirestone2339@gmail.com>
2021-05-27 11:13:32 -0700

    Initial commit

---
+++ src/containers/LikesContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import DisplayLikes from '../components/DisplayLikes'
+import LikeButton from '../components/LikeButton'
+
+export default function LikesContainer(props) {
+    return (
+        <div>
+            <DisplayLikes likes={props.likes} />
+            <LikeButton 
+                likeCollection={props.likeCollection}
+                collectionId={props.collectionId}
+                likes={props.likes} 
+            />
+        </div>
+    )
+}
